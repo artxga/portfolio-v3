0309d2a1f6074eefe21ee1196457c088a395458c
artxga <angelarteaga989@gmail.com>
2025-07-26 18:26:10 -0500

    gb ig

--- src/core/static/social-bar.ts
+++ src/core/static/social-bar.ts
@@ -2,11 +2,6 @@ import { Github, Instagram, Linkedin, Mail } from "lucide-react";
 
 export const socialBarData = [
   {
-    id: "instagram",
-    icon: Instagram,
-    url: "https://www.instagram.com/ssh.angel_/",
-  },
-  {
     id: "linkedin",
     icon: Linkedin,
     url: "https://www.linkedin.com/in/angelart179/",
