8f55a43f5dd2179771306917bb69630376b79d34
artxga <angelarteaga989@gmail.com>
2026-05-11 21:06:43 -0500

    improvements

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -1,12 +1,16 @@
+"use client"
+
 import { experiences } from "@/core/static/experiences";
 import ExperienceItem from "./experience-item";
 import SectionTitle from "./section-title";
+import { useLanguage } from "@/context/language-context";
 
 export default function ExperienceSection() {
+  const { language } = useLanguage()
 
   return (
     <section id="experience" className="flex flex-col justify-center gap-9 pt-18">
-      <SectionTitle title="Experience" />
+      <SectionTitle title={language === "en" ? "Experience" : "Experiencia"} />
       <div className="flex flex-col ml-3">
         {
           experiences.map((item, index) => (
