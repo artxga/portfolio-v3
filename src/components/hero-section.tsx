c40154261e291a81fcabba5a82f6f7bb741821ad
artxga <angelarteaga989@gmail.com>
2026-08-01 00:42:06 -0500

    fix: add top margin to hero section on mobile to prevent overlap with header

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
 
   return (
     <div className="top-0 flex flex-col justify-center h-screen col-start-1 col-end-13 md:sticky md:col-start-2 md:col-span-4 2xl:col-span-3 2xl:col-start-3">
-      <div className="flex flex-col gap-16 h-fit">
+      <div className="flex flex-col gap-16 h-fit md:mt-0 mt-[63px]">
         <div className="flex flex-col gap-4">
           <h1 className="text-4xl font-bold md:text-5xl text-[var(--foreground)]">
             {heroData.name}
